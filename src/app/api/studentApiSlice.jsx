e06e74cf263cb395eb1530c6aee1426dafd22d5e
s.segunshowunmi@gmail.com <s.segunshowunmi@gmail.com>
2024-10-06 13:56:53 -0700

    ...

--- src/app/api/studentApiSlice.jsx
+++ src/app/api/studentApiSlice.jsx
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../../studentAuthSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3500",
-  //baseUrl: "https://sams-backend-1.onrender.com",
+  //baseUrl: "http://localhost:3500",
+  baseUrl: "https://sams-backend-1.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().studentAuth.token;
